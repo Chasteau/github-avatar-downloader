f30616cddbd3a429081a4f57f8ab2432742f42a0
Chasteau <davidchasteau@gmail.com>
2017-06-05 21:11:01 +0000

    working

--- download_avatars.js
+++ download_avatars.js
@@ -2,58 +2,35 @@
 //In this activity, you will partially implement a single function called getRepoContributors. Using the request package, you will fetch the list of contributors as a JSON string from the GitHub API's contributors endpoint. Upon receiving the results, your function will invoke a callback function with the results. This callback function will loop over and print out the avatar_url for each object in the collection.
 
 // require nodes request
+var API_KEY = require("../API_KEY");
 var request = require('request');
-//require nodes filesys
-var fs = require('fs');
-//import api key
-var API_KEY = require("../API_KEY")
 
-function printAvatarUrl(error, response, body) {
+function printAvatarUrl(error, response) {
+	var json = JSON.parse(response.body);
   if (error) {
     console.error(error);
   } else if (response.statusCode == 503) {
     console.error('Server error');
   } else {
-//	  else if (response.statusCode == 200) {
-    var json = JSON.parse(body);
-
-    // for (var i = 0; i < json.data.length; i++) {
-    //   var currentBeer = json.data[i];
-    //   displayBeer(currentBeer);
-    // }
-
-    //json.data.forEach(vatar_url)
-	  console.log(json);
-  }
+	  for (i = 0; i < json.length; i++) {
+		  console.log(json[i].avatar_url);
+	  }
+  } 
 }
 
 function getRepoContributors(repoOwner, repoName, cb) {
 // Token 
 var GITHUB_USER = "Chasteau";
 var GITHUB_TOKEN = API_KEY;
-var requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
-	
-request(requestURL, processData);
-	
-//console.log(requestURL);
-//	request.get(requestURL)   
-//       .on('error', function (err) { 
-//		console.log("Errors:", err);
-//       })
-//       .on('response', function (response) {
-////		console.log(JSON.parse(response.body))
-//        cb(response.body); 
-//	}
-//		.on('end', function (response) {
-//		console.log('complete');
-//       })
-//		  )};
-//       .pipe(fs.createWriteStream('./downloaded.html'));  
-//	console.log(resposne);
+var userAgent = "--User-Agent: Chasteau"  
+var requestURL =` https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+	var options = {
+		url: requestURL,
+		headers: {
+			'User-Agent': userAgent
+		}
+	}
+request(options, cb);
 }
 
-// This callback function will loop over and print out the avatar_url for each object in the collection.
-
-
 getRepoContributors("jquery", "jquery", printAvatarUrl);
-//getRepoContributors("jquery", "jquery", printAvatarUrl);
